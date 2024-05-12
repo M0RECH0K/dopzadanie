29c26497635671160e1082f163bf8d9ff183bd93
Dmitry Kharlamov <168891004+M0RECH0K@users.noreply.github.com>
2024-05-12 10:28:31 +0300

    Add files via upload

---
+++ dopzadanie.py
@@ -0,0 +1,22 @@
+Students = ['Aaron', 'Bilbo', 'Johnny', 'Khendrik', 'Steve']
+Grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+Grades[0] = (5 + 3 + 3 + 5 + 4)/5
+Grades[1] = (2 + 2 + 2 + 3)/4
+Grades[2] = (4 + 5 + 5 + 2)/4
+Grades[3] = (4 + 4 + 3)/3
+Grades[4] = (5 + 5 + 5 + 4 + 5)/5
+Students[0] = Grades[0]
+Students[1] = Grades[1]
+Students[2] = Grades[2]
+Students[3] = Grades[3]
+Students[4] = Grades[4]
+name = input('Введите имя студента чтобы увидеть его средний балл ')
+print(Students[0])
+name = input('Введите имя студента чтобы увидеть его средний балл ')
+print(Students[1])
+name = input('Введите имя студента чтобы увидеть его средний балл ')
+print(Students[2])
+name = input('Введите имя студента чтобы увидеть его средний балл ')
+print(Students[3])
+name = input('Введите имя студента чтобы увидеть его средний балл ')
+print(Students[4])
